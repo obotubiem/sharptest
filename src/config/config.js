47b4c68dddc4f4fb79f36b4295c28b3f0dec0e28
Arief Budiman <rief.website@gmail.com>
2022-11-25 20:12:40 +0700

    fix

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
+  use_env_variable: false,
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME ?? '127.0.0.1',
+    host: process.env.DB_HOSTNAME ?? "127.0.0.1",
     port: process.env.DB_PORT ?? 5432,
     dialect: process.env.DB_DIALECT,
   },
