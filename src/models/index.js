fc8e3fba3209169890b1ba46aab6e1d6acb5d523
Arief Budiman <rief.website@gmail.com>
2022-11-25 20:30:05 +0700

    fix

--- src/models/index.js
+++ src/models/index.js
@@ -5,10 +5,10 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV ?? "development";
-const config = require(path.resolve(__dirname, "../", "config/config.js"))[env];
+const config = require("../config/config")[env];
 const db = {};
 console.log(config);
-console.log(path.resolve(__dirname, "../", "config/config.js"))
+console.log(path.resolve(__dirname, "../", "config/config.js"));
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 fs.readdirSync(__dirname)
