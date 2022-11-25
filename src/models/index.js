ac72d75bac6fc6cb27696f39b807bdc860cd54bb
Arief Budiman <rief.website@gmail.com>
2022-11-25 20:16:26 +0700

    fix

--- src/models/index.js
+++ src/models/index.js
@@ -4,10 +4,10 @@ const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "development";
+const env = process.env.NODE_ENV ?? "development";
 const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
-
+console.log(config);
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 fs.readdirSync(__dirname)
