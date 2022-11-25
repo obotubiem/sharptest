6b1e3924048414b84095224650573b00a83a9d17
Arief Budiman <rief.website@gmail.com>
2022-11-25 20:34:32 +0700

    fix

--- src/config/config.js
+++ src/config/config.js
@@ -9,4 +9,12 @@ module.exports = {
     port: process.env.DB_PORT ?? 5432,
     dialect: process.env.DB_DIALECT,
   },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME ?? "127.0.0.1",
+    port: process.env.DB_PORT ?? 5432,
+    dialect: process.env.DB_DIALECT,
+  },
 };
--- src/models/index.js
+++ src/models/index.js
@@ -5,12 +5,9 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV ?? "development";
-const config = require("../config/config");
+const config = require("../config/config")[env];
 const db = {};
 console.log(config);
-console.log(env);
-console.log(config[env]);
-console.log('itu bukan confignya');
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 fs.readdirSync(__dirname)
